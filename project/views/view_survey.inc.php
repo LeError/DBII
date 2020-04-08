a8e2a50e8f593d85467979e9f13f4cdc813d2217
moritzbuerkle <buerkle.moritz@web.de>
2020-04-09 00:44:47 +0200

    loop question html

--- project/views/view_survey.inc.php
+++ project/views/view_survey.inc.php
@@ -1,14 +1,17 @@
+<?php
+/**
+ * view_survey.inc.php
+ *
+ * Create Survey
+ *
+ * @author     Moritz Bürkle
+ */
+?>
+
 <div class="ui container">
 
     <br>
-
-    form
-
-    Form for creating a survey, title short, title, number of questions
-
-    @author     Moritz Bürkle
-
-    <form method="post" action="../logic/createSurvey.php">
+    <form method="post" action="index.php">
         <table width="50%" border="0"  cellspacing="10px">
             <tr>
                 <th align="left" colspan="2">Create survey</th>
@@ -26,9 +29,41 @@
                 <td><input  min="1" value="1" name="numberOfQuestions" type="number" /></td>
             </tr>
             <tr>
-
                 <td align="left" colspan="2"><input type="submit" value="Create questions" name="createQuestion"></td>
             </tr>
         </table>
+
+        <?php
+        if(isset($_POST['createQuestion'])){
+            createQuestionsHTML();
+
+        }
+
+        function createQuestionsHTML(){
+            echo "
+            <table width='50%' border='0'  cellspacing='10px'>
+            <tr>
+                <th align='left' colspan='2'>Create questions</th>
+            </tr>
+            ";
+            $numberOfQuestions = $_POST['numberOfQuestions'];
+
+            for($y = 1; $y < $numberOfQuestions+1; $y++){
+                echo "
+                <tr>
+                <td>Frage {$y}:</td>
+                <td><input maxlength='100' name='frage{$y}' type='text' size='45'/></td>
+                </tr>
+                 ";
+            }
+            echo "
+            <tr>
+                <td align='left' colspan='2'><input type='submit' value='Create survey' name='createSurvey'></td>
+            </tr>
+            </table>
+            ";
+
+        }
+        ?>
     </form>
 </div>
