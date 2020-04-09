e596553ffa9aa551720800b059f9c497c2701820
D073980 <robin.herder@sap.com>
2020-04-09 15:27:15 +0200

    base input checks for login / registration

--- project/js/login.js
+++ project/js/login.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
                     {
                         type: 'empty',
                         prompt: 'Please enter your Username'
+                    },
+                    {
+                        type   : 'length[6]',
+                        prompt : 'Your Username must be at least 6 characters long'
                     }
                 ]
             },
@@ -16,6 +20,10 @@ $(document).ready(function () {
                     {
                         type: 'empty',
                         prompt: 'Please enter your password'
+                    },
+                    {
+                        type   : 'length[8]',
+                        prompt : 'Your password must be at least 8 characters long'
                     }
                 ]
             }
