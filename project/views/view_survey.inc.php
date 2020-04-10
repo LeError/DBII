44b9e676917cba4b1e5c7ac0955600f778c3c4a1
malik <malik4privat@gmail.com>
2020-04-10 13:01:02 +0200

    ui added

--- project/views/view_survey.inc.php
+++ project/views/view_survey.inc.php
@@ -1,3 +1,30 @@
-<div class="ui container">
+     <!--
+     * index.php
+     *
+     * Survey View
+     *
+     * @author     Malik Press
+     * -->
 
+<div class="ui grid">
+       <div class="ui sixteen wide column">
+            <button class="ui positive labeled icon button" style="margin-top: 3vh"><i class="plus icon"></i>Neue Umfrage erstellen</button>
+       </div>
+       <div class="ui sixteen wide column">
+            <h3 class="ui header">Meine Umfragen</h3>
+       </div>
+       <div class="ui sixteen wide column">
+            <div class="bd-survey-list-item" style="padding-bottom: 0.75vh;">
+                <button class="ui button" style="width: 50vw;">Titel</button>
+                <button class="ui inverted secondary icon button"><i class="edit icon"></i></button>
+                <button class="ui inverted secondary icon button"><i class="copy icon"></i></button>
+                <button class="ui inverted red icon button"><i class="trash icon"></i></button>
+            </div>
+            <div class="bd-survey-list-item" style="padding-bottom: 0.75vh;">
+                <button class="ui button" style="width: 50vw;">Titel</button>
+                <button class="ui inverted secondary icon button"><i class="edit icon"></i></button>
+                <button class="ui inverted secondary icon button"><i class="copy icon"></i></button>
+                <button class="ui inverted red icon button"><i class="trash icon"></i></button>
+            </div>
+       </div>
 </div>
