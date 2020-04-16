83bc1caacc40d6b5187f57fbf4380e7663922aa3
D073980 <robin.herder@sap.com>
2020-04-16 16:01:03 +0200

    Added Basic Request Handler

--- project/logic/requestHandler.req.php
+++ project/logic/requestHandler.req.php
@@ -1,5 +1,8 @@
 <?php
-    if(isset($_POST['ul_user']) && isset($_POST['ul_pass'])) {
+    if(isset($_POST['ul_user']) && isset($_POST['ul_pass']) && isset($_POST['ul_register']) && $_POST['ul_register'] == true) {
+        registerUser($_POST['ul_user'], $_POST['ul_pass']);
+        loginUser($_POST['ul_user'], $_POST['ul_pass']);
+    } else if(isset($_POST['ul_user']) && isset($_POST['ul_pass'])) {
         loginUser($_POST['ul_user'], $_POST['ul_pass']);
     }
     if(isset($_GET['logout'])) {
