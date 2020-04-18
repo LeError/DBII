c544cc0f2e3aafe48aa556f8cdf55678aec2fbb6
D073980 <robin.herder@sap.com>
2020-04-18 10:04:09 +0200

    updated request handler

--- project/logic/requestHandler.req.php
+++ project/logic/requestHandler.req.php
@@ -10,7 +10,6 @@
     //Login / Register User
     if(isset($_POST['ul_user']) && isset($_POST['ul_pass']) && isset($_POST['ul_register']) && $_POST['ul_register'] == true) {
         registerUser($_POST['ul_user'], $_POST['ul_pass']);
-        loginUser($_POST['ul_user'], $_POST['ul_pass']);
     } else if(isset($_POST['ul_user']) && isset($_POST['ul_pass'])) {
         loginUser($_POST['ul_user'], $_POST['ul_pass']);
     }
