2413d6d7e53f056ffa249e09cb26720d095d567e
D073980 <robin.herder@sap.com>
2020-04-24 13:16:02 +0200

    Added Dialogue for creating a course

---
+++ project/views/view_new_course.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * view_new_course.inc.php
+ *
+ * View Form for Creating a course
+ *
+ * @author     Robin Herder
+ */
+
+?>
+
+<div class="ui one column grid center aligned" style="margin-top: 30px">
+    <div class="column">
+        <div class="ui raised segment left aligned">
+            <div class="ui red ribbon label">Create</div>
+            <span>Neuen Kurs anlegen</span>
+            <form id="create_course" action="index.php?view=new_course" method="post" class="ui large form" style="margin: 25px 10px 10px;">
+                <div class="field">
+                    <div class="ui labeled input">
+                        <div class="ui label">
+                            K&uuml;rzel
+                        </div>
+                        <input maxLength="8" type="text" name="c_course_short" placeholder="WWIBE118">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="ui labeled input">
+                        <div class="ui label">
+                            Kurs
+                        </div>
+                        <input maxLength="50" type="text" name="c_course" placeholder="WWI118 Business Engineering">
+                    </div>
+                </div>
+                <button class="ui fluid large teal button submit" type="submit">Erstellen</button>
+                <div class="ui error message"></div>
+            </form>
+        </div>
+    </div>
+</div>
