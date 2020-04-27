72fdde2e41a7bd9e591d22476811e474a209b4ce
lera0207 <pusteblume.2013@web.de>
2020-04-27 13:13:56 +0200

    Survey Übersichtseite mit Links Zu Edit und Copy

--- project/views/view_survey.inc.php
+++ project/views/view_survey.inc.php
@@ -10,7 +10,7 @@ require ('./logic/survey.req.php');
 ?>
 <div class="ui grid">
        <div class="ui sixteen wide column right aligned">
-           <a href="index.php?view=create_survey">
+           <a href="?index.php?view=create_survey">
                 <form method="post">
                      <button class="ui positive labeled icon button" name="action" type="submit" value="new" style="margin-top: 3vh"><i class="plus icon"></i>Neue Umfrage erstellen</button>
                 </form>
@@ -20,20 +20,26 @@ require ('./logic/survey.req.php');
             <h3 class="ui header">Meine Umfragen</h3>
        </div>
        <div class="ui sixteen wide column">
-           <form method="post">
            <?php
                 $name  = 'Malik';
                 $data = getSurveyRecords($name);
                 for ($i = 0; $i < count($data);$i++) {
                     echo '<div class="bd-survey-list-item" style="padding-bottom: 0.75vh;">
                 <button class="ui button" name="action" type="submit" value="survey'. $i .'" style="width: 50vw;">' . $data[$i] . '</button>
-                <button class="ui inverted secondary icon button" name="action" type="submit" value="edit'. $i .'"><i class="edit icon"></i></button>
-                <button class="ui inverted secondary icon button" name="action" type="submit" value="copy'. $i .'"><i class="copy icon"></i></button>
+                <form method="post" action ="index.php?view=edit_survey"> <button class="ui inverted secondary icon button" name="edit" type="submit" value="edit'. $i .'"><i class="edit icon"></i></button></form>
+               <form method="post" action ="index.php?view=copy_survey">  <button class="ui inverted secondary icon button" name="copy" type="submit" value="copy'. $i .'"><i class="copy icon"></i></button></form>
                 <button class="ui inverted red icon button" name="action" type="submit" value="delete'. $i .'"><i class="trash icon"></i></button>
             </div>';
                 };
            ?>
-           </form>
        </div>
     <br>
 </div>
+
+
+<?php
+/*Idee zur Übergabe des Titel, Malik? 
+if(isset($_POST["edit"])){
+    $_SESSION['title'] = ($_POST["edit"]);
+    echo $_SESSION['title']; }
+
